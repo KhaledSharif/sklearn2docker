6474829b789dd18721491ad9c9b2101f1b7d60b5
Khaled Sharif <kldsrf@gmail.com>
2018-02-19 13:06:44 +0200

    Fixed unit tests; you can now test inside the 'tests' using the 'unittest' Python library

--- tests/decision_tree_test.py
+++ tests/decision_tree_test.py
@@ -1,35 +1,47 @@
-# create Sklearn classifier
-from pandas import DataFrame, read_json
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+import unittest
 
-iris = load_iris()
-input_df = DataFrame(data=iris['data'], columns=iris['feature_names'])
-clf = DecisionTreeClassifier(max_depth=2)
-clf.fit(input_df.values, iris['target'])
 
-# convert classifier to Docker container
-from sklearn2docker.constructor import Sklearn2Docker
-s2d = Sklearn2Docker(
-    classifier=clf,
-    feature_names=iris['feature_names'],
-    class_names=iris['target_names'].tolist(),
-)
-s2d.save(
-    name="classifier",
-    tag="iris",
-)
+class DecisionTreeUnitTest(unittest.TestCase):
+    def test_decision_tree(self):
+        from requests import post
+        from os import system
+        from pandas import DataFrame, read_json
+        from sklearn.datasets import load_iris
+        from sklearn.tree import DecisionTreeClassifier
 
-# run your Docker container as a detached process
-from os import system
-system("docker run -d -p 5000:5000 classifier:iris && sleep 5")
+        iris = load_iris()
+        input_df = DataFrame(data=iris['data'], columns=iris['feature_names'])
+        clf = DecisionTreeClassifier(max_depth=2)
+        clf.fit(input_df.values, iris['target'])
 
-# send your training data as a json string
-from requests import post
-request = post("http://localhost:5000/predict/split", json=input_df.to_json(orient="split"))
-result = read_json(request.content.decode(), orient="split")
-print(result.head())
+        # convert classifier to Docker container
+        from sklearn2docker.constructor import Sklearn2Docker
+        s2d = Sklearn2Docker(
+            classifier=clf,
+            feature_names=iris['feature_names'],
+            class_names=iris['target_names'].tolist(),
+        )
+        s2d.save(
+            name="classifier",
+            tag="iris",
+        )
 
-request = post("http://localhost:5000/predict_proba/split", json=input_df.to_json(orient="split"))
-result = read_json(request.content.decode(), orient="split")
-print(result.head())
+        # run your Docker container as a detached process
+        system("docker run -d -p 5000:5000 --name sklearn2docker_unittest classifier:iris && sleep 5")
+
+        # send your training data as a json string
+        request = post("http://localhost:5000/predict/split", json=input_df.to_json(orient="split"))
+        result = read_json(request.content.decode(), orient="split")
+        self.assertEqual(list(result), ['prediction'])
+        self.assertGreater(len(result), 0)
+
+        request = post("http://localhost:5000/predict_proba/split", json=input_df.to_json(orient="split"))
+        result = read_json(request.content.decode(), orient="split")
+        self.assertEqual(list(result), ['setosa', 'versicolor', 'virginica'])
+        self.assertGreater(len(result), 0)
+
+        system("docker kill sklearn2docker_unittest")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/keras_classifier_test.py
+++ tests/keras_classifier_test.py
@@ -1,24 +1,33 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.datasets import load_iris
-from pandas import DataFrame
-from numpy import array
 
 
-def create_model():
-	model = Sequential()
-	model.add(Dense(12, input_dim=4, activation='relu'))
-	model.add(Dense(8, activation='relu'))
-	model.add(Dense(3, activation='sigmoid'))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+import unittest
 
 
-iris = load_iris()
-input_df = DataFrame(data=iris['data'], columns=iris['feature_names'])
+class DecisionTreeUnitTest(unittest.TestCase):
+    @staticmethod
+    def create_model():
+        from keras.models import Sequential
+        from keras.layers import Dense
 
-model = KerasClassifier(build_fn=create_model, epochs=5, verbose=1)
-X, Y = input_df.values, array(iris['target'])
+        model = Sequential()
+        model.add(Dense(12, input_dim=4, activation='relu'))
+        model.add(Dense(8, activation='relu'))
+        model.add(Dense(3, activation='sigmoid'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
 
-model.fit(X, Y)
+    def test_decision_tree(self):
+        from keras.wrappers.scikit_learn import KerasClassifier
+        from sklearn.datasets import load_iris
+        from pandas import DataFrame
+        from numpy import array
+
+        iris = load_iris()
+        input_df = DataFrame(data=iris['data'], columns=iris['feature_names'])
+        model = KerasClassifier(build_fn=self.create_model, epochs=5, verbose=1)
+        X, Y = input_df.values, array(iris['target'])
+        model.fit(X, Y)
+
+
+
+
